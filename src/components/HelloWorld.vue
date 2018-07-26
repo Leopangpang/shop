e9f97cf4769f8bab9d994d185fafec0c72e7173a
Leopangpang <450720503@qq.com>
2018-07-26 19:58:01 +0800

    新建vue商城项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+   <div>
+      <!-- <van-loading color="black" />
+      <van-loading color="white" />
+      <van-loading type="spinner" color="black" />
+      <van-loading type="spinner" color="white" /> -->
+  </div>
+
+  </div>
+</template>
+
+<script>
+// import { Loading } from 'vant'
+export default {
+  name: 'HelloWorld',
+  //  components: {
+  //   [Loading.name]: Loading
+  // },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/pages/index/Index.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="index-area">
+      <!-- 轮播区域 -->
+      <div class="index-swiper">
+        <van-swipe :autoplay="3000">
+          <van-swipe-item>
+            <img src="../../assets/images/lunbo1.jpg"/>
+          </van-swipe-item>
+          <van-swipe-item>
+            <img src="../../assets/images/lunbo2.jpg"/>
+          </van-swipe-item>
+          <van-swipe-item>
+            <img src="../../assets/images/lunbo3.jpg"/>
+          </van-swipe-item>
+          <van-swipe-item>
+            <img src="../../assets/images/lunbo4.jpg"/>
+          </van-swipe-item>
+        </van-swipe>
+      </div>
+      <!-- 介绍区域 -->
+      <div class="index-section introduce-section">
+         <div class="is-top">
+           <ul class="clearfloat">
+             <li>
+               <i class="shopIcon shop-newtag index-color-yellow"></i>
+               <span>New</span>
+             </li>
+             <li>
+               <i class="shopIcon shop-huangguan index-color-grey"></i>
+               <span>Brand</span>
+             </li>
+             <li>
+               <i class="shopIcon shop-sale  index-color-qing"></i>
+               <span>Sale</span>
+             </li>
+             <li>
+               <i class="shopIcon shop-zhuanshi index-color-lv"></i>
+               <span>Boutique</span>
+             </li>
+           </ul>
+         </div>
+         <div class="is-bottom">
+           <div class="ib-item">
+             <div class="ibi-img ibi-left">
+               <img src="../../assets/images/lunbo1.jpg"/>
+             </div>
+             <div class="ibi-img">
+               <img src="../../assets/images/lunbo2.jpg"/>
+             </div>
+           </div>
+            <div class="ib-item">
+             <div class="ibi-img ibi-left">
+               <img src="../../assets/images/lunbo3.jpg"/>
+             </div>
+             <div class="ibi-img">
+               <img src="../../assets/images/lunbo4.jpg"/>
+             </div>
+           </div>
+         </div>
+         <p>
+           <span>More</span>
+           <i class="shopIcon shop-shuangjiantou-right-bold"></i>
+         </p>
+      </div>
+
+    </div>
+</template>
+
+<script>
+import { Swipe, SwipeItem } from 'vant'
+export default{
+  components: {
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem
+  },
+  data () {
+    return {
+      msg: 'asdfas'
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.index-area{
+  .index-color-yellow{
+    color: #D6B17B;
+  }
+  .index-color-grey{
+    color: #BFBFBF;
+  }
+  .index-color-qing{
+    color: #ADCDD7;
+  }
+  .index-color-lv{
+    color: #ACCAAA;
+  }
+  // 轮播区域
+  .index-swiper{
+    width:100%;
+    font-size: 0;
+    img{
+      width:100%;
+    }
+  }
+  .index-section{
+    padding: 0.5rem 0;
+    position: relative;
+    >p{
+      bottom: 0;
+      right: 2%;
+      margin-top: 0.5rem;
+      display: inline-block;
+      position: absolute;
+    }
+  }
+  // 介绍区域
+  .introduce-section{
+    .is-top{
+      overflow: hidden;
+      padding-top:0.5rem;
+      ul{
+        li{
+          float:left;
+          width:25%;
+          text-align: center;
+          i{
+            font-size: 2.5rem;
+          }
+          span{
+            display: block;
+            font-size: 0.9rem;
+            padding-top:0.2rem;
+          }
+        }
+      }
+    }
+    .is-bottom{
+      padding:1rem 0;
+      .ib-item{
+        font-size: 0;
+        margin-bottom: 0.5rem;
+        padding:0 2%;
+        .ibi-img{
+          width:49%;
+          display: inline-block;
+          img{
+            width:100%;
+            border-radius: 0.5rem;
+          }
+        }
+        .ibi-left{
+          margin-right:2%;
+        }
+      }
+    }
+  }
+}
+</style>
